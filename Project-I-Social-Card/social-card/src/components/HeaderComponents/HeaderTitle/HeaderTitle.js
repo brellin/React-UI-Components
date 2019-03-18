0ed7560607987d7c8a4838b790facc3288c139a1
brellin <cekakid@gmail.com>
2019-03-18 16:59:30 -0500

    replaced '|' with superscript period

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle/HeaderTitle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 
 const HeaderTitle = () => {
-    return <h1>Lambda School <i>@LambdaSchool | {moment("0318", "MMDD").format('D MMM')}</i></h1>;
+    return <h1>Lambda School <i>@LambdaSchool <sup>.</sup> {moment("0318", "MMDD").format('D MMM')}</i></h1>;
 }
 
 export default HeaderTitle;
